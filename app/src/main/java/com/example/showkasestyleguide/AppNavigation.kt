2903ecaa9eb4ff28a8599b767d7933a57f521a1a
Mike Hudov <gudov.m2@gmail.com>
2025-07-02 12:18:04 +0100

    small refactoring

--- app/src/main/java/com/example/showkasestyleguide/AppNavigation.kt
+++ app/src/main/java/com/example/showkasestyleguide/AppNavigation.kt
@@ -1,6 +1,5 @@
 package com.example.showkasestyleguide
 
-import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
